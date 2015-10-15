1899d4ee458e42fc419818d1dbcea80f97758eb8
M-UNIT <mugabevedward@gmail.com>
2015-10-15 16:46:24 +0300

    catch and try

---
+++ src/nerima/catchtry.java
@@ -0,0 +1,19 @@
+package nerima;
+
+public class catchtry {
+
+	public static void main(String[] args) {
+		
+			 
+			try{
+			int a[] = new int[2];
+			System .out.println("Access elem ent three :" + a[3]);
+			}catch(ArrayIndexOutOfBoundsException e){
+			System .out.println("Exception thrown :" + e);
+			}
+			System .out.println("Out of the block");
+// TODO Auto-generated method stub
+
+	}
+
+}
